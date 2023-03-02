9682df1252daba30add855b821e5048c127e8098
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-02 17:17:30 +0300

    first add JavaLec11.java

---
+++ JavaLec1/JavaLec11.java
@@ -0,0 +1,19 @@
+package JavaLec1;
+
+/**
+ * program
+ */
+// public class program {
+//     public static void main(String[] args) {
+//         System.out.println("world");
+//     }
+    
+// }
+public class JavaLec11 {
+    public static void main(String[] args) {
+        String s = null;
+
+        System.out.println(s);
+    }
+
+}
