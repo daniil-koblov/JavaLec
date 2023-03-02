908fef1971f796684cb998bdd5e2b5b9e71001fd
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-02 17:23:46 +0300

    first add JavaLec12.java

---
+++ JavaLec1/JavaLec12.java
@@ -0,0 +1,18 @@
+package JavaLec1;
+
+// public class JavaLec12 {
+//     public static void main(String[] args) {
+//         short age = 10;
+//         int salary = 123456;
+//         System.out.println(age); //10
+//         System.out.println(salary); //123456
+//     }
+//    }
+public class JavaLec12 {
+    public static void main(String[] args) {
+        float f = 123.45f;
+        double d = 123.45;
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
