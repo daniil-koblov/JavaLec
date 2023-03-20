d300a5bcea920c4085a4e0016e3a17321a3baa94
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-20 12:44:13 +0300

    first add JavaLec51.java

--- JavaLec5/JavaLec51.java
+++ JavaLec5/JavaLec51.java
@@ -1,5 +1,17 @@
 package JavaLec5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JavaLec51 {
-    
+    public static void main(String[] args) {
+        Map<Integer, String> db = new HashMap<>();
+        db.put(1, "один"); System.out.println(db);
+        db.put(2, "два"); System.out.println(db);
+        db.put(3, "три"); System.out.println(db);
+        db.put(31, "три один"); System.out.println(db);
+        db.put(13, "один три"); System.out.println(db);
+        db.put(null, "!null"); System.out.println(db);
+        db.put(null, null); System.out.println(db);
+        }       
 }
