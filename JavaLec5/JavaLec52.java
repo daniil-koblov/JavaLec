0ecfb9db368827d1fb129a8952f2c29553ca5402
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-20 12:49:58 +0300

    second add JavaLec52.java

--- JavaLec5/JavaLec52.java
+++ JavaLec5/JavaLec52.java
@@ -1,5 +1,18 @@
 package JavaLec5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JavaLec52 {
-    
+    public static void main(String[] args) {
+        Map<Integer, String> db = new HashMap<>();
+        db.putIfAbsent(1, "один");
+        db.put(2, "два");
+        db.put(3, "три");
+        System.out.println(db);
+        for (var item : db.entrySet()) {
+        System.out.printf("[%d: %s]\n", item.getKey(), item.getValue());
+        }
+        }
+       
 }
