487ae9c9dbc253809aef737588e4148026cadaee
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-20 12:56:19 +0300

    first add JavaLec56.java

--- JavaLec5/JavaLec56.java
+++ JavaLec5/JavaLec56.java
@@ -1,5 +1,15 @@
 package JavaLec5;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class JavaLec56 {
-    
+    public static void main(String[] args) {
+        Map<Integer,String> table = new Hashtable<>();
+        table.put(1, "два");
+        table.put(11, "один один");
+        table.put(2, "один");
+        System.out.println(table); // {2=один, 1=два, 11=один один}
+        // table.put(null, "один"); // java.lang.NullPointerException
+        }
 }
