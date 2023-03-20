aa24f923bda67521ac3b80b971fae9f8f8977e93
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-03-20 12:54:14 +0300

    first add JavaLec55.java

--- JavaLec5/JavaLec55.java
+++ JavaLec5/JavaLec55.java
@@ -1,5 +1,20 @@
 package JavaLec5;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class JavaLec55 {
-    
+    public static void main(String[] args) {
+        Map<Integer,String> linkmap = new LinkedHashMap<>();
+        linkmap.put(11, "один один");
+        linkmap.put(1, "два");
+        linkmap.put(2, "один");
+        System.out.println(linkmap); // {11=один один, 1=два, 2=один}
+        Map<Integer,String> map = new HashMap<>();
+        map.put(11, "один один");
+        map.put(2, "два");
+        map.put(1, "один");
+        System.out.println(map); // {1=один, 2=два, 11=один один}
+        }   
 }
